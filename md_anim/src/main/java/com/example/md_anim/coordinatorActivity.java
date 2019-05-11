fea4051c9ac1040893edcebd16b7e71771a328e3
Wubingyu <15150503671@163.com>
2019-05-11 21:22:40 +0800

    动画在自定View中练习中做了。这一个分支暂时废弃，以后或许继续在这里深入了解MD动画

---
+++ md_anim/src/main/java/com/example/md_anim/coordinatorActivity.java
@@ -0,0 +1,13 @@
+package com.example.md_anim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class coordinatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coordinator);
+    }
+}
